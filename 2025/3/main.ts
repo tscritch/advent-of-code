c5429765b1b25e66acd19079a9c4f758b226ac9b
Tad Scritchfield <tadscritch@gmail.com>
2025-12-02 23:18:15 -0600

    day 3 part 1

--- 2025/3/main.ts
+++ 2025/3/main.ts
@@ -1,4 +1,34 @@
 import { readInput } from "../utils";
 
 const input = await readInput("./3/input.txt");
-console.log(input);
+// const input = `987654321111111\n811111111111119\n234234234234278\n818181911112111`;
+// console.log(input);
+
+const lines = input.split("\n");
+
+const joltage = lines.reduce((acc: number, line: string) => {
+  let left = 0;
+  let left_idx = 0;
+  // do less than full length cause we can't have a left digit as the last in the line
+  for (let i = 0; i < line.length - 1; i++) {
+    if (Number(line[i]) > left) {
+      left = Number(line[i]);
+      left_idx = i;
+    }
+  }
+
+  let right = 0;
+  for (let i = left_idx + 1; i < line.length; i++) {
+    if (Number(line[i]) > right) {
+      right = Number(line[i]);
+    }
+  }
+
+  const j = Number(`${left}${right}`);
+  console.log(j, left, right);
+
+  acc += j;
+  return acc;
+}, 0);
+
+console.log(`Total: ${joltage}`);
